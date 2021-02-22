9b3b1b1ee19d4d4e2494bee6835d6bed89b43a0e
neeraj-iitm <mailneerajsahu@gmail.com>
2021-02-22 20:40:36 +0530

    init

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true , useUnifiedTopology: true }); 
+//mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true , useUnifiedTopology: true }); 
+mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    }).then(res=>{
+            console.log("DB Connected!")
+    }).catch(err => {
+      console.log(Error, err.message);
+    })
+
 const connection = mongoose.connection;
 const collegesRouter = require("./routes/colleges");
 app.use("/colleges", collegesRouter);
